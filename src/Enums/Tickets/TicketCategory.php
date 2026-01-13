8781b9cd83088673966797429094f7c566f4a875
Tschigo <tschigo@gmail.com>
2026-01-13 14:13:39 +0100

    Update ticket categories with new options

--- src/Enums/Tickets/TicketCategory.php
+++ src/Enums/Tickets/TicketCategory.php
@@ -4,5 +4,12 @@ namespace SmartDato\Desk365\Enums\Tickets;
 
 enum TicketCategory: string
 {
-    case Pickup = '🚚 Pickup';
+    case PICKUP = '🚚 Pickup';
+    case DELIVERY = '📦 Delivery';
+    case SPECIAL_REQUEST = '📣 Special Request';
+    case PAYMENT_INVOIVE = '💳 Payment & Invoive';
+    case EPC_QUOTE = '📑 EPC quote';
+    case GENERAL_INFORMATION = 'ℹ️ General information';
+    case OTHER = '💬 Other';
+
 }
